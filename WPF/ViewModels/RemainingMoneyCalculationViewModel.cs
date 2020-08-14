5aed7c4a6b3825e5f7914d6de52f7b103953c92b
egoken8017 <egoken8017@gmail.com>
2020-08-14 09:58:49 +0900

    BaseViewModel、RemainingMoneyCalculationView、RemainingMoneyCalculationViewModel作成

---
+++ WPF/ViewModels/RemainingMoneyCalculationViewModel.cs
@@ -0,0 +1,76 @@
+﻿namespace WPF.ViewModels
+{
+    public class RemainingMoneyCalculationViewModel:BaseViewModel
+    {
+         public int TenThousandYenCount
+        {
+            get => tenThousandYenCount;
+            set
+            {
+                tenThousandYenCount = value;
+                TenThousandYenAmountWithUnit = $"{value * 10000:N0)} 円";
+                CallPropertyChanged();
+            }
+        }
+           
+        public string TenThousandYenAmountWithUnit
+        {
+            get => tenThousandYenAmountWithUnit;
+            set
+            {
+                tenThousandYenAmountWithUnit = value;
+                CallPropertyChanged();
+            }
+        }
+      
+        public int FiveThousandYenCount
+        {
+            get => fiveThousandYenCount;
+            set
+            {
+                fiveThousandYenCount = value;
+                FiveThousandYenAmountWithUnit = $"{value * 5000:N0} 円";
+                CallPropertyChanged();
+            }
+        }
+       
+        public string FiveThousandYenAmountWithUnit
+        {
+            get => fiveThousandYenAmountWithUnit;
+            set
+            {
+                fiveThousandYenAmountWithUnit = value;
+                CallPropertyChanged();
+            }
+        }
+
+        public int OneThousandYenCount
+        {
+            get => oneThousandYenCount;
+            set
+            {
+                oneThousandYenCount = value;
+                OneThousandYenAmountWithUnit = $"{value*1000:N0} 円";
+                CallPropertyChanged();
+            }
+        }
+
+        public string OneThousandYenAmountWithUnit
+        {
+            get => oneThousandYenAmountWithUnit;
+            set
+            {
+                oneThousandYenAmountWithUnit = value;
+                CallPropertyChanged();
+            }
+        }
+        
+        private string tenThousandYenAmountWithUnit;
+        private int tenThousandYenCount;
+        private int fiveThousandYenCount;
+        private string oneThousandYenAmountWithUnit;
+        private string fiveThousandYenAmountWithUnit;
+        private int oneThousandYenCount;
+
+    }
+}
